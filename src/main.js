c2e012a6e0e2d5fabc0c7043052b5c75c449d887
Timur <akkord3034@gmail.com>
2023-05-13 00:31:13 +0500

    Добавляет моковые данные, модели и отображение новых данных

--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
 import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
 import TripInfoView from './view/trip-info.js';
-import {render} from './render.js';
-import {RenderPosition} from './render.js';
+import {render, RenderPosition} from './render.js';
 import TripPresenter from './presenter/trip-presenter.js';
+
+import MockService from './service/mock-service.js';
+import DestinationsModel from './model/destinations-model.js';
+import OffersModel from './model/offers-model.js';
+import PointsModel from './model/points-model.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteTripEventElement = document.querySelector('.trip-events');
 const siteTripMainElement = document.querySelector('.trip-main');
-const tripPresenter = new TripPresenter({tripContainer: siteTripEventElement});
+
+const mockService = new MockService();
+const destinationsModel = new DestinationsModel(mockService);
+const offersModel = new OffersModel(mockService);
+const pointsModel = new PointsModel(mockService);
+
+
+const tripPresenter = new TripPresenter({
+  tripContainer: siteTripEventElement,
+  destinationsModel,
+  offersModel,
+  pointsModel
+});
 
 render(new TripInfoView(), siteTripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), siteFilterElement);
--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -1,6 +1,34 @@
 import {createElement} from '../render.js';
+import {POINT_EMPTY} from '../const.js';
+import {CITIES} from '../mock/const.js';
+import dayjs from 'dayjs';
 
-function createEditFormTemplate() {
+
+function createEditFormTemplate({point, pointDestinations, pointOffers}) {
+  const {basePrice, type, destination, dateFrom, dateTo} = point;
+
+  function createPictureTemplate(pictures) {
+    return pictures.map((picture) =>
+      `<img class="event__photo" src="${picture.src}" alt="Event photo">`).join('');
+  }
+
+  const isChecked = (offer) => point.offers.includes(offer.id) ? 'checked' : '';
+
+
+  const concreteOffers = pointOffers.find((offer) => offer.type === type).offers;
+
+  const offersList = concreteOffers
+    .map((offer) => `
+      <div class="event__offer-selector">
+        <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${isChecked(offer)}>
+        <label class="event__offer-label" for="event-offer-luggage-1">
+          <span class="event__offer-title">${offer.title}</span>
+          &plus;&euro;&nbsp;
+          <span class="event__offer-price">${offer.price}</span>
+        </label>
+      </div>`)
+    .join('');
+
   return (`
 <li class="trip-events__item">
   <form class="event event--edit" action="#" method="post">
@@ -8,7 +36,7 @@ function createEditFormTemplate() {
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${point.type}.png" alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -66,22 +94,20 @@ function createEditFormTemplate() {
 
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
+          ${point.type}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${pointDestinations.find((el) => el.id === destination).name}" list="destination-list-1">
         <datalist id="destination-list-1">
-          <option value="Amsterdam"></option>
-          <option value="Geneva"></option>
-          <option value="Chamonix"></option>
+        ${CITIES.map((city) => (`<option value="${city}"></option>`)).join(' ')}
         </datalist>
       </div>
 
       <div class="event__field-group  event__field-group--time">
         <label class="visually-hidden" for="event-start-time-1">From</label>
-        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
+        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dayjs(dateFrom).format('DD/MM/YY HH:mm')}">
         &mdash;
         <label class="visually-hidden" for="event-end-time-1">To</label>
-        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
+        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dayjs(dateTo).format('DD/MM/YY HH:mm')}">
       </div>
 
       <div class="event__field-group  event__field-group--price">
@@ -89,7 +115,7 @@ function createEditFormTemplate() {
           <span class="visually-hidden">Price</span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
+        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
@@ -103,56 +129,18 @@ function createEditFormTemplate() {
         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
         <div class="event__available-offers">
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-            <label class="event__offer-label" for="event-offer-luggage-1">
-              <span class="event__offer-title">Add luggage</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">50</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-            <label class="event__offer-label" for="event-offer-comfort-1">
-              <span class="event__offer-title">Switch to comfort</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">80</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-            <label class="event__offer-label" for="event-offer-meal-1">
-              <span class="event__offer-title">Add meal</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">15</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-            <label class="event__offer-label" for="event-offer-seats-1">
-              <span class="event__offer-title">Choose seats</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">5</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-            <label class="event__offer-label" for="event-offer-train-1">
-              <span class="event__offer-title">Travel by train</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">40</span>
-            </label>
-          </div>
-        </div>
+          ${offersList}
       </section>
 
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-        <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
+        <p class="event__destination-description">${pointDestinations.find((el) => el.id === destination).description}</p>
+
+        <div class="event__photos-container">
+            <div class="event__photos-tape">
+            ${createPictureTemplate(pointDestinations.find((el) => el.id === destination).pictures)}
+            </div>
+        </div>
       </section>
     </section>
   </form>
@@ -162,8 +150,19 @@ function createEditFormTemplate() {
 }
 
 export default class EditFormView {
+
+  constructor({point = POINT_EMPTY, pointDestinations, pointOffers}) {
+    this.point = point;
+    this.pointDestinations = pointDestinations;
+    this.pointOffers = pointOffers;
+  }
+
   getTemplate() {
-    return createEditFormTemplate();
+    return createEditFormTemplate({
+      point: this.point,
+      pointDestinations: this.pointDestinations,
+      pointOffers: this.pointOffers
+    });
   }
 
   getElement() {
